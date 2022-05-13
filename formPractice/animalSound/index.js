b43ca805a58bff0068eb874ffef943f38c5184d6
sojeongsojeong-park <psj90972@naver.com>
2022-05-14 00:21:34 +0900

    make button action

--- formPractice/animalSound/index.js
+++ formPractice/animalSound/index.js
@@ -4,8 +4,20 @@ const app = document.getElementById("app");
 const button = document.querySelector("button");
 button.addEventListener("click", function(e){
   e.preventDefault;
+  button.style.display = "none"
   app.innerHTML = `
-    
+    <div class="section dogSection">
+      <p>🐶</p>
+      <input type="text" placeholder="what's my name?">
+    </div>
+    <div class="section catSection">
+      <p>🐱</p>
+      <input type="text" placeholder="what's my name?">
+    </div>
+    <div class="section mouseSection">
+      <p>🐭</p>
+      <input type="text" placeholder="what's my name?">
+    </div>
   `
 })
 
